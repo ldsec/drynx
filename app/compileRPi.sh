9e9673a48b150350fa73eca0987d1811f839f769
John Stephan <john.stephan@epfl.ch>
2019-03-18 18:41:50 +0100

    quick commit

--- app/compileRPi.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-env GOOS=linux GOARCH=arm GOARM=5 go build -tags vartime -o drynxRPi *.go
--- app/vns.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-./drynx server setupNonInteractive --serverBinding "10.90.39.21:2000" --description "Drynx VN 1" \
-        --privateTomlPath "test/vn1-private.toml" --publicTomlPath "test/vn1-public.toml"
-./drynx server setupNonInteractive --serverBinding "10.90.39.22:2000" --description "Drynx VN 2" \
-        --privateTomlPath "test/vn2-private.toml" --publicTomlPath "test/vn2-public.toml"
-./drynx server setupNonInteractive --serverBinding "10.90.39.23:2000" --description "Drynx VN 3" \
-        --privateTomlPath "test/vn3-private.toml" --publicTomlPath "test/vn3-public.toml"
-cat test/vn*-public.toml > "test/groupVNs.toml"
